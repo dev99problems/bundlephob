5b55d2f93a462c6201e0e2b5464158b5c7289681
Eugene Chulkov <genechulkov@gmail.com>
2019-10-04 19:34:38 +0300

    add pre-commit prettier fix; remove unused cli dep

--- prettier.config.js
+++ prettier.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  "arrowParens": "avoid",
-  "bracketSpacing": false,
-  "insertPragma": false,
-  "printWidth": 80,
-  "proseWrap": "preserve",
-  "quoteProps": "as-needed",
-  "requirePragma": false,
-  "semi": false,
-  "singleQuote": true,
-  "tabWidth": 2,
-  "trailingComma": "none",
-  "useTabs": false
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  insertPragma: false,
+  printWidth: 80,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
 }
