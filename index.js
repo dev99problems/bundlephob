a1b9108578c6d6b9d90aaa1378a04df9a8479965
Eugene Chulkov <genechulkov@gmail.com>
2019-10-16 19:36:28 +0300

    add minor workflow description changes

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-(async () => {
+;(async () => {
   const alfy = require('alfy')
-  
+
   const packageQuery = process.argv[2] || ''
-  const isQueryEmpty = !(!!packageQuery.trim().length)
+  const isQueryEmpty = !!!packageQuery.trim().length
 
   if (isQueryEmpty) {
     alfy.output(alfy.cache.get('last-search-results'))
     return
   }
 
-  const data = await alfy.fetch(`https://api.npms.io/v2/search/suggestions?q=${packageQuery}`) || []
+  const data =
+    (await alfy.fetch(
+      `https://api.npms.io/v2/search/suggestions?q=${packageQuery}`
+    )) || []
 
   const items = data.map(element => {
     const { name, description, version } = (element || {}).package || {}
     const packageTitle = `${name}@${version}`
 
-    return ({
+    return {
       title: packageTitle,
       subtitle: description,
+      autocomplete: packageTitle,
       arg: packageTitle
-    })
+    }
   })
 
   alfy.cache.set('last-search-results', items)
